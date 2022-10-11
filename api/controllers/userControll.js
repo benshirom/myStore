10041ec5f066b0935d77aa1ca89b841f89d92d1c
benshirom <57137329+benshirom@users.noreply.github.com>
2022-10-11 18:51:36 +0300

    c

--- api/controllers/userControll.js
+++ api/controllers/userControll.js
@@ -12,6 +12,25 @@ exports.userCtrl={
           console.log(err)
           res.status(500).json({msg:"err",err})
         }  
+      },
+      deleteUser: async(req,res) => {
+        try{
+          let delId = req.params.delId;
+          let userInfo;
+          
+          if(req.tokenData.role == "admin"){
+            userInfo = await UserModel.deleteOne({_id:delId});
+          }
+          else{
+            //,{name:1,email:1}
+            userInfo = await UserModel.deleteOne({_id:req.tokenData._id},{password:0});
+          }
+            res.json(userInfo);
+        }
+        catch(err){
+          console.log(err)
+          res.status(500).json({msg:"err",err})
+        }  
       }
       ,userList:async(req,res) => {
         try{
--- api/routes/users.js
+++ api/routes/users.js
@@ -12,5 +12,6 @@ router.post("/",authCtrl.signUp)
 router.post("/login", authCtrl.login)
 
 router.get("/myInfo",auth,userCtrl.myInfo)
+router.delete("/",auth,userCtrl.deleteUser)
 router.get("/usersList", authAdmin ,userCtrl.userList)
 module.exports = router;
