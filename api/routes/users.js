da661f147bc40b9a6496db3072d83a2cca8537aa
benshirom <57137329+benshirom@users.noreply.github.com>
2022-10-17 18:01:35 +0300

    v

--- api/routes/users.js
+++ api/routes/users.js
@@ -8,6 +8,6 @@ router.post("/", authCtrl.signUp)
 router.post("/login", authCtrl.login)
 
 router.get("/myInfo", auth, userCtrl.myInfo)
-router.delete("/", auth, userCtrl.deleteUser)
+router.delete("/:delId", auth, userCtrl.deleteUser)
 router.get("/usersList", authAdmin, userCtrl.userList)
 module.exports = router;
